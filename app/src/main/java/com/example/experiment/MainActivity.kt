59bd662380287b5a3136c7eebfff1e8ea2b3b483
Abhishek karn <proabhishek732@gmail.com>
2021-07-13 11:02:16 +0545

    Toast message is added

--- app/src/main/java/com/example/experiment/MainActivity.kt
+++ app/src/main/java/com/example/experiment/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.experiment
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,8 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun regard(){
-
-        }
+        Toast.makeText(this, "Hey Abhishek", Toast.LENGTH_SHORT).show()
     }
 }
